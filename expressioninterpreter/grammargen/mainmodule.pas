8bf09320ac3329df897db633a5fbd41a13e684cd
Martin Schreiber <mse00000@gmail.com>
2013-03-29 17:15:46 +0100

    * grammargen.

--- expressioninterpreter/grammargen/mainmodule.pas
+++ expressioninterpreter/grammargen/mainmodule.pas
@@ -7,28 +7,52 @@ uses
 type
  tmainmo = class(tmsedatamodule)
    sysenv: tsysenvmanager;
-   procedure initsysenvexe(sender: tsysenvmanager);
+   procedure eventloopexe(const sender: TObject);
+   procedure afterinitexe(sender: tsysenvmanager);
  end;
 var
  mainmo: tmainmo;
 implementation
 uses
- mainmodule_mfm;
+ mainmodule_mfm,msefileutils,msestream,msesys;
 type
  paramty = (pa_grammarfile,pa_pasfile,pa_incfile);
-const
- params: array[paramty] of argumentdefty = (
-  (kind: ak_pararg; name: 'g';  anames: nil;  flags: [];
-           initvalue: '';),
-  (kind: ak_pararg; name: 'p';  anames: nil;  flags: [];
-           initvalue: '';),
-  (kind: ak_pararg; name: 'i';  anames: nil;  flags: [];
-           initvalue: '';)
- );
- 
-procedure tmainmo.initsysenvexe(sender: tsysenvmanager);
+
+procedure creategrammar(const grammar,pas,inc: filenamety);
+var
+ grammarstream: ttextstream = nil;
+ passtream: ttextstream = nil;
+ incstream: ttextstream = nil;
 begin
- sender.init(params);
+ try
+  grammarstream:= ttextstream.create(grammar,fm_read);
+  passtream:= ttextstream.create(pas,fm_create);
+  incstream:= ttextstream.create(pas,fm_create);
+ finally
+  grammarstream.free;
+  passtream.free;
+  incstream.free;
+ end;
+end;
+
+procedure tmainmo.eventloopexe(const sender: TObject);
+begin
+ with sysenv do begin
+  creategrammar(value[ord(pa_grammarfile)],value[ord(pa_pasfile)],
+                                                 value[ord(pa_incfile)]);
+ end;
+end;
+
+procedure tmainmo.afterinitexe(sender: tsysenvmanager);
+begin
+ with sender do begin
+  if not defined[ord(pa_pasfile)] then begin
+   value[ord(pa_pasfile)]:= replacefileext(value[ord(pa_grammarfile)],'pas');
+  end;
+  if not defined[ord(pa_incfile)] then begin
+   value[ord(pa_incfile)]:= replacefileext(value[ord(pa_grammarfile)],'inc');
+  end;
+ end;
 end;
 
 end.
