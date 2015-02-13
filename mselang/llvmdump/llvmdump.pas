76445050b7ba5ab957c3d99977750abad3a42bbe
Martin Schreiber <mse00000@gmail.com>
2015-02-13 11:37:52 +0100

    + llvmbcreader. + llvmdump.

---
+++ mselang/llvmdump/llvmdump.pas
@@ -0,0 +1,8 @@
+program llvmdump;
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+{$ifdef mswindows}{$apptype console}{$endif}
+uses
+ {$ifdef FPC}{$ifdef unix}cthreads,cwstring,{$endif}{$endif}
+ sysutils,llvmbcreader;
+begin
+end.
