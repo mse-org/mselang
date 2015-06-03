c1886bd98c238c0e1a3afe21802d536f0ccc2c8e
Martin Schreiber <mse00000@gmail.com>
2015-06-03 16:13:31 +0200

    * rtunitreader read uses.

--- mselang/rtunitreader.pas
+++ mselang/rtunitreader.pas
@@ -25,7 +25,7 @@ function readunitfile(const aunit: punitinfoty): boolean; //true if ok
 implementation
 uses
  filehandler,segmentutils,msestream,msestrings,msesys,msesystypes,globtypes,
- msearrayutils,elements;
+ msearrayutils,elements,sysutils;
  
 function readunitfile(const aunit: punitinfoty): boolean; //true if ok
 var
@@ -35,56 +35,94 @@ var
  names1,anons1: identarty;
  pd,pe: pint32;
  ns,ne: pchar;
- po2: pointer;
+ poend: pointer;
+ po3: plenidentty;
+ idmin1,idmax1: int32;
+ 
+ procedure updateident(var aident: int32);
+ begin
+  if (aident < idmin1) or (aident > idmax1) then begin
+   abort();
+  end;
+  if aident >= 0 then begin
+   aident:= names1[aident];
+  end
+  else begin
+   aident:= anons1[1-aident];
+  end;
+ end; //updateident
+
+ procedure getdata(var source: plenidentty; out dest: identarty);
+ var
+  ps,pd,pe: pidentty;
+ begin
+  allocuninitedarray(source^.len,sizeof(identty),dest);
+  ps:= @source^.data;
+  pe:= ps+source^.len;
+  if pointer(pe) > poend then begin
+   abort();
+  end;
+  pd:= pointer(dest);
+  while ps < pe do begin
+   pd^:= ps^;
+   updateident(int32(pd^));
+   inc(pd);
+   inc(ps);
+  end;
+  source:= pointer(pe);
+ end; //getdata
+
+var
+ interfaceuses1,implementationuses1: identarty;
 begin
  result:= false;
  fna1:= getrtunitfile(aunit^.name);
  if (fna1 <> '') and 
        (tmsefilestream.trycreate(stream1,fna1,fm_read) = sye_ok) then begin    
   try
-   resetsegment(seg_unitintf);
-   resetsegment(seg_unitidents);
-   result:= checksegmentdata(stream1,aunit^.filetimestamp) and
-             readsegmentdata(stream1,[seg_unitintf,seg_unitidents{,seg_op}]);
-   if result then begin
-    if getsegmentsize(seg_unitintf) < sizeof(unitintfinfoty) then begin
-     exit; //invalid
-    end;
-    po1:= getsegmentbase(seg_unitintf);
-    try
-     allocuninitedarray(po1^.header.anoncount,sizeof(identty),anons1);
-    except
-     exit;
-    end;
-    pd:= pointer(anons1);
-    pe:= pd + length(anons1);
-    while pd < pe do begin
-     pd^:= getident();
-     inc(pd);
-    end;
-    try
-     allocuninitedarray(po1^.header.namecount,sizeof(identty),names1);
-    except
-     exit;
-    end;
-    pd:= pointer(names1);
-    pe:= pd + length(names1);
-    ne:= getsegmentbase(seg_unitidents);
-    po2:= pointer(ne) + getsegmentsize(seg_unitidents);     
-    while pd < pe do begin
-     ns:= @pidentstringty(ne)^.data;
-     ne:= ns + pidentstringty(ne)^.len;
-     if ne > po2 then begin
+   try
+    resetsegment(seg_unitintf);
+    resetsegment(seg_unitidents);
+    result:= checksegmentdata(stream1,aunit^.filetimestamp) and
+              readsegmentdata(stream1,[seg_unitintf,seg_unitidents{,seg_op}]);
+    if result then begin
+     if getsegmentsize(seg_unitintf) < sizeof(unitintfinfoty) then begin
       exit; //invalid
      end;
-     pd^:= getident(ns,ne);
-     inc(pd);
+     po1:= getsegmentbase(seg_unitintf);
+     allocuninitedarray(po1^.header.anoncount,sizeof(identty),anons1);
+     pd:= pointer(anons1);
+     pe:= pd + length(anons1);
+     while pd < pe do begin
+      pd^:= getident();
+      inc(pd);
+     end;
+     allocuninitedarray(po1^.header.namecount,sizeof(identty),names1);
+     pd:= pointer(names1);
+     pe:= pd + length(names1);
+     ne:= getsegmentbase(seg_unitidents);
+     poend:= pointer(ne) + getsegmentsize(seg_unitidents);     
+     while pd < pe do begin
+      ns:= @pidentstringty(ne)^.data;
+      ne:= ns + pidentstringty(ne)^.len;
+      if ne > poend then begin
+       exit; //invalid
+      end;
+      pd^:= getident(ns,ne);
+      inc(pd);
+     end;
+     idmin1:= -length(anons1);
+     idmax1:= high(names1);
+     po3:= @po1^.interfaceuses;
+     getdata(po3,interfaceuses1);
+     getdata(po3,implementationuses1);
     end;
+   finally
+    stream1.destroy();
+    resetsegment(seg_unitintf);
+    resetsegment(seg_unitidents);
    end;
-  finally
-   stream1.destroy();
-   resetsegment(seg_unitintf);
-   resetsegment(seg_unitidents);
+  except
   end;
  end;
 end;
